68501c294c919ac1007fc9c1ac6a8e15a35ce8fe
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-02-09 14:26:14 +0200

    Added my name and my section

--- src/labofdatastructure/Triangle.java
+++ src/labofdatastructure/Triangle.java
@@ -1,18 +1,21 @@
+//Name : محمود قناوي محمود أبو عمر.
+//Section : 12
+
 package labofdatastructure;
 
 import java.applet.Applet;
 import java.awt.Graphics;
 
 public class Triangle extends Applet {
-    
+
     int[] x = {20, 50, 80};
     int[] y = {80, 20, 80};
-    
+
+    @Override
     public void paint(Graphics g) {
         g.drawPolygon(x, y, 3);
     }
-    
+
     //Note -> When running this program there is some error will appear 
     //but don't care it's working
-    
 }
